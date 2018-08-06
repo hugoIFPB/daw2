8795512b7e2af5bda9e0806cee323c3a5570538a
hugof <hugof@192.168.15.9>
2018-08-06 00:53:34 -0300

    nova funcionalidade

--- IFoto/src/main/java/br/edu/ifpb/esperanca/daw2/dao/UsuarioDAO.java
+++ IFoto/src/main/java/br/edu/ifpb/esperanca/daw2/dao/UsuarioDAO.java
@@ -1,7 +1,14 @@
 package br.edu.ifpb.esperanca.daw2.dao;
 
+import java.util.List;
+
+import br.edu.ifpb.esperanca.daw2.filtros.UsuarioFiltro;
 import br.edu.ifpb.esperanca.daw2.ifoto.entities.Usuario;
 
 public class UsuarioDAO extends DAO<Usuario> {
 
+	public List<Usuario> findBy(UsuarioFiltro filtro) {
+		return null;
+	}
+	
 }
